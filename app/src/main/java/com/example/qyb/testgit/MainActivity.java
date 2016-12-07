281ca126dedcf7c688a9521d6ffa85e6774630ab
yubo <634966673@qq.com>
2016-12-07 10:12:01 +0800

    testgit

--- app/src/main/java/com/example/qyb/testgit/MainActivity.java
+++ app/src/main/java/com/example/qyb/testgit/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     public static String TAG="TestGit";
     public static String TAG2="TestGit2";
     public static String TAG3="TestGit3";
+    private static String localTag="TestLocalTag";
 
 
     @Override
