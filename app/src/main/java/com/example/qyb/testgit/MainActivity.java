c3d246b73f9c5936c2695177d520a1d63c95d667
scrystally <yuboqiu@foxmail.com>
2016-12-07 10:16:01 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/qyb/testgit/MainActivity.java
+++ app/src/main/java/com/example/qyb/testgit/MainActivity.java
@@ -8,8 +8,7 @@ public class MainActivity extends AppCompatActivity {
     public static String TAG="TestGit";
     public static String TAG2="TestGit2";
     public static String TAG3="TestGit3";
-    private static String localTag="TestLocalTag";
-    public static String remoteTag="TestRemoteTag";
+    private static String otherTag="otherTag";
 
 
     @Override
