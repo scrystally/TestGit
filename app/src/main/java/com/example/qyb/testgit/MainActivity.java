4e9b288851361a8d24fa18f48a75d6f30194333b
yubo <634966673@qq.com>
2016-12-07 09:36:23 +0800

    testgit

--- app/src/main/java/com/example/qyb/testgit/MainActivity.java
+++ app/src/main/java/com/example/qyb/testgit/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String TAG="TestGit";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
